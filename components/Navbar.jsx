2d1b86540ce525852f402c1173a605d8fd242647
Jean-Yves <jy.babiarczyk@gmail.com>
2023-08-04 10:05:31 +0200

    first commit

---
+++ components/Navbar.jsx
@@ -0,0 +1,52 @@
+import Link from "next/link";
+import { AioutlineMenu, AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
+
+export default function Navbar() {
+  return (
+    <div className="fixed left-0 top-0 w-full z-10 ease-in duration-300">
+      <div className="max-w-[1240px] m-auto flex justify-between items-center p-4 text-white">
+        <Link href="/">
+          <h1 className="font-bold text-4xl">Captur</h1>
+        </Link>
+        <ul className="hidden sm:flex">
+          <li className="p-4">
+            <Link href="/">Home</Link>
+          </li>
+          <li className="p-4">
+            <Link href="/#gallery">Gallery</Link>
+          </li>
+          <li className="p-4">
+            <Link href="/portfolio">Work</Link>
+          </li>
+          <li className="p-4">
+            <Link href="/contact">Contact</Link>
+          </li>
+        </ul>
+
+        {/* Mobile Button */}
+        <div className="block sm:hidden z-10">
+          <AiOutlineMenu size={25} />
+        </div>
+        {/* End Mobile Button */}
+
+        {/* Mobile Menu */}
+        <div>
+          <ul>
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/#gallery">Gallery</Link>
+            </li>
+            <li>
+              <Link href="/portfolio">Work</Link>
+            </li>
+            <li>
+              <Link href="/contact">Contact</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
