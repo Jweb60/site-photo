2d5b52a158450dd23c315163ff643067008b629f
Jean-Yves <jy.babiarczyk@gmail.com>
2023-08-04 10:23:45 +0200

    navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,14 +1,39 @@
+'use client';
+
 import Link from "next/link";
+import { useEffect, useState } from "react";
 import { AioutlineMenu, AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 export default function Navbar() {
+
+    const[nav,SetNav] = useState(false);
+    const [color,setColor] = useState('transparent');
+    const [textColor,setTextColor] = useState('white');
+
+    const handleNav = () => {
+        SetNav(!nav);
+    };
+
+    useEffect(()=>{
+        const changeColor = () => {
+            if(window.scrollY >= 90) {
+                setColor('#ffffff')
+                setTextColor('#000000')
+            } else {
+                setColor('transparent')
+                setTextColor('white')
+            }
+        }
+        window.addEventListener('scroll',changeColor)
+    },[])
+
   return (
-    <div className="fixed left-0 top-0 w-full z-10 ease-in duration-300">
+    <div style={{backgroundColor: `${color}`}} className="fixed left-0 top-0 w-full z-10 ease-in duration-300">
       <div className="max-w-[1240px] m-auto flex justify-between items-center p-4 text-white">
         <Link href="/">
-          <h1 className="font-bold text-4xl">Captur</h1>
+          <h1 style={{color: `${textColor}`}} className="font-bold text-4xl">Captur</h1>
         </Link>
-        <ul className="hidden sm:flex">
+        <ul style={{color: `${textColor}`}} className="hidden sm:flex">
           <li className="p-4">
             <Link href="/">Home</Link>
           </li>
@@ -24,24 +49,29 @@ export default function Navbar() {
         </ul>
 
         {/* Mobile Button */}
-        <div className="block sm:hidden z-10">
-          <AiOutlineMenu size={25} />
+        <div onClick={handleNav} className='block sm:hidden z-10'>
+          {nav ? (
+            <AiOutlineClose size={20} style={{ color: `${textColor}` }} />
+          ) : (
+            <AiOutlineMenu size={20} style={{ color: `${textColor}` }} />
+          )}
         </div>
-        {/* End Mobile Button */}
 
         {/* Mobile Menu */}
-        <div>
+        <div className={ nav ? 
+            "sm:hidden absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-300" : 
+            "sm:hidden absolute top-0 left-[-100%] right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-300"}>
           <ul>
-            <li>
+            <li onClick={handleNav} className="p-4 text-4xl hover:text-gray-500">
               <Link href="/">Home</Link>
             </li>
-            <li>
+            <li onClick={handleNav} className="p-4 text-4xl hover:text-gray-500">
               <Link href="/#gallery">Gallery</Link>
             </li>
-            <li>
+            <li onClick={handleNav} className="p-4 text-4xl hover:text-gray-500">
               <Link href="/portfolio">Work</Link>
             </li>
-            <li>
+            <li onClick={handleNav} className="p-4 text-4xl hover:text-gray-500">
               <Link href="/contact">Contact</Link>
             </li>
           </ul>
